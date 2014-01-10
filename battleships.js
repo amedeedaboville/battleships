1f8413b0ada29f0710fa8c0e7263b17d6d9f01ad
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-01-10 11:47:56 -0500

    Initial commit. Already have user account functionality set up.

---
+++ battleships.js
@@ -0,0 +1,29 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to battleships.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+    console.log("You pressed the button");
+    }
+  });
+
+  sendChat = function(message) {
+    chatStream.emit('message', message);
+    console.log('me: ' + message);
+  };
+
+  chatStream.on('message', function(message) {
+    console.log('user: ' + message);
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+chatStream = new Meteor.Stream('chat');
---
+++ client/admin.js
@@ -0,0 +1,8 @@
+//Meteor.loginAsAdmin = function(password, callback) {
+//  var loginRequest = {admin: true, password: password};
+//
+//  Accounts.callLoginMethod({
+//    methodArguments: [loginRequest],
+//    userCallback: callback
+//  });
+//};
