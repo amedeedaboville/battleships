506648338c651b66f8bee5e74c513eb6c885e979
Leonardo Florez <leonardoldd@gmail.com>
2014-01-17 08:34:54 -0500

    added changes from testing branch (ugly user-status for online users)

--- client/client.js
+++ client/client.js
@@ -3,6 +3,23 @@ Template.lobby.greeting = function () {
   return "Welcome to battleships.";
 };
 
+Template.lobby.username = function () {
+
+  return Meteor.user().username;
+
+};
+
+Template.lobby.onlineUsers = function () {
+  Meteor.subscribe("userStatus");
+  var races = Meteor.users.find({"status.online": true}, {username:true}).fetch();
+  var s = "";
+  for (var i=0; i<races.length; i++) {
+    s = s + races[i].username + ", "
+  console.log(s);
+  }
+  return s;
+};
+
 var trimInput = function(val) {
   //from SO: The part to the left matches any leading spaces (^), the part to the right matches any trailing space ($).
   //The g modifier allows this matching to be applied more than once. Then we have selected all the whitespace and we replace it
@@ -43,7 +60,7 @@ Template.register.events({
     var name = trimInput(t.find('#register-name').value);
 
     console.log("Register with email " + email + " and password " + password + " and name " + name);
-    Accounts.createUser({email: email, password : password, name: name}, function (err) {
+    Accounts.createUser({email: email, password : password, username: name}, function (err) {
       if (err) {
         console.log("Registration failed with err");
         console.log(err);
@@ -56,7 +73,7 @@ Template.register.events({
   }
 });
 
-Template.hello.events({
+Template.lobby.events({
   'click input' : function () {
     // template data, if any, is available in 'this'
     if (typeof console !== 'undefined')
