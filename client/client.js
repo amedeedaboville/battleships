e128e76d293e3c453598e1f464530f22b4ba2fdb
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-02-04 14:51:00 -0500

    Added grid. Also shows bases and coral

--- client/client.js
+++ client/client.js
@@ -21,6 +21,48 @@ Template.lobby.helpers({
 
 });
 
+function getRandomInt (min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+Template.grid.helpers({
+  rows: function () {
+    var grid = [];
+    //build the sea
+    for(var i = 0; i < 30; i++) {
+      grid[i] = [];
+      for( j = 0; j < 30; j++) {
+        grid[i].push("sea");
+      }
+    }
+    for(var i = 0; i < 10; i++) {
+      grid[i+10][0]  = "bluebase";
+      grid[i+10][29] = "redbase";
+    }
+    
+    var coralSpots = [];
+    while(coralSpots.length < 24) {
+      var newSpot = [getRandomInt(3,26), getRandomInt(10,20)];
+      var spotAlreadyPicked = false;
+      for(var i = 0; i < coralSpots.length; i++) {
+        if(coralSpots[i][0] == newSpot[0] && coralSpots[i][1] == newSpot[1]) {
+          spotAlreadyPicked  = true;
+          break;
+        }
+      }
+      if (!spotAlreadyPicked) {
+        coralSpots.push(newSpot);
+        grid[newSpot[0]][newSpot[1]] = "coral";
+
+      }
+    }
+    console.log(coralSpots);
+//return [
+//       ['sea', 'sea', 'sea'], 
+//       ['sea', 'sea', 'sea'], 
+//       ['sea', 'sea', 'sea']
+//       ]
+    return grid;
+}});
 
 
 Template.lobby.events({
