46418a03e2a781afd4e99085f13394c87130afd3
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-02-13 17:12:30 -0500

    Moved map/map.js to game/game.js

--- client/game/game.js
+++ client/game/game.js
@@ -1,6 +1,7 @@
 Deps.autorun(function (){
-    Meteor.subscribe('games', Meteor.userId());
-    var currentGame = gameCollection.findOne({active: true}, {});
+    //Meteor.subscribe('games', Meteor.userId());
+    //var currentGame = gameCollection.findOne({active: true}, {});
+    var currentGame = new Game(Meteor.userId(), );
     if (currentGame) {
 	if (!currentGame.active)
 	{
