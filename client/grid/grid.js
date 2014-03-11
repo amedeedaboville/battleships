fef3fe8ed68b62598e080d697dfd3e2186b2100c
charliewright <wrightcharlesh@gmail.com>
2014-03-11 18:35:35 -0400

    Completely reorganized grid, map, and gridClass files. Added pseudocode to ship class.

--- client/grid/grid.js
+++ client/grid/grid.js
@@ -1,14 +1,10 @@
-function getRandomInt (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 Template.grid.helpers({
-    rows: function () {
-        return new Map().grid;
-        //return Session.get("Map").grid;
-    }
+  rows: function () {
+          return new Map().grid.grid;
+          //return Session.get("Map").grid;
+        }
 });
 
 Template.grid.rendered = function(){
-    console.log('Grid rendered');
+  console.log('Grid rendered');
 }
