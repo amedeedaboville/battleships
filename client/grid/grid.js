3a5955e69e71c8bef04fbbb3ada7193620c81569
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-03-11 22:27:45 -0400

    Fixed connectivity issues. Properly created the map. Ships are now visibles changed ship constructor. Added hack to set prototype of map after pulling it out of the collection.

--- client/grid/grid.js
+++ client/grid/grid.js
@@ -1,8 +1,9 @@
 Template.grid.helpers({
     rows: function () {
         var game = Session.get('currentGame');
-        if (game != undefined){
-        return game.map.grid.grid;
+        if (game != undefined) {
+            game.map.__proto__ = new Map(); //get instance methods back
+            return game.map.getSquares();
         }
     }
 });
