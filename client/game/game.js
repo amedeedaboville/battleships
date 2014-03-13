1daf85c688db9db76e387a5a70cbfac1c8ea0879
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-03-13 16:21:30 -0400

    Removed old code and changed action name to 'text'

--- client/game/game.js
+++ client/game/game.js
@@ -3,39 +3,43 @@ Deps.autorun(function (){
     //Meteor.subscribe('games', Meteor.userId());
     //var currentGame = gameCollection.findOne({active: true}, {});
     if (currentGame) {
-	if (!currentGame.active)
-	{
-		//delete Session.keys["enemy"];
-        //hide the game elements
-		$('#mapModal').modal('hide');
-	}
-    else if (!currentGame.mapAccepted) {
-        $('#mapModal').modal();
-	}
-	else if (currentGame.mapAccepted)
-	{
+        if (!currentGame.active) {
+            //delete Session.keys["enemy"];
+            //hide the game elements
+            $('#mapModal').modal('hide');
+        }
+        else if (!currentGame.mapAccepted) {
+            $('#mapModal').modal();
+        }
+        else if (currentGame.mapAccepted) {
             //Success
-		$('#mapModal').modal('hide');
-        Session.set('inGame', true);
-        if(currentGame.turn == Meteor.userId()) {
-            Session.set('currentTurn', true);
-        }
-        else {
-            Session.set('currentTurn', false);
-        }
-        //Draw the actual game
+            $('#mapModal').modal('hide');
+            Session.set('inGame', true);
+            if(currentGame.turn == Meteor.userId()) {
+                Session.set('currentTurn', true);
+            }
+            else {
+                Session.set('currentTurn', false);
+            }
+            //Draw the actual game
 
-	}
+        }
     }
 });
 
 Template.contextMenu.helpers({
-   possibleMoves : function() {
-       test = ["Move","Rotate"];
-       console.log("current game is ");
-       console.log(Session.currentGame);
-       return test;
-//       return currentGame.moveArray;
+    possibleMoves : function() {
+        console.log("calculating possible moves");
+        test = ["Move","Rotate"];
+        var ship = Session.get('selectedShip');
+        if(ship) {
+            console.log("ship exists, looking for actions:");
+            var actions =  ship.actions.map(function(a) { return a.name});
+            console.log(actions);
+            return acions;
+        }
+
+        return test;
     },
     currentTurn: function() {
         return Session.get('currentTurn');
@@ -44,6 +48,5 @@ Template.contextMenu.helpers({
     selectedShip: function(){
         return Session.get('selectedShip');
     },
-
 });
 
