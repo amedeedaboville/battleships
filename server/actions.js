09e4ec638cf6ff5d4f73f6fc30fbbb0e2414b502
John Saigle <john.saigle@gmail.com>
2014-03-14 03:27:13 -0400

    added actions.js functionality for fireCannon including logging.

--- server/actions.js
+++ server/actions.js
@@ -33,5 +33,22 @@ Meteor.methods({
     turnShip: function(gameID, ship, position) {
         console.log("Got request to turn ship.");
     }
+    fireCannon: function(gameID, ship, targetPosition){
+        console.log("Got request in game" +gameID+" to KILL A MOTHERFUCKER AT POSITION "+targetPosition+" WITH A FUCKING CANNON AUUAARRGGHHHH")
+            var game = gameCollection.findOne({_id:gameID});
+            map.grid.__proto__ = new Grid();
+            if (game){
+                map.__proto__ = new Map();
+                map.fireCannon(ship, targetPosition);
+
+                console.log("done with the map operation.");
+
+                gameCollection.update({_id:gameID}, game);
+                console.log("done updating game, square should be shot")
+            } else {
+                console.log("Error game does not exist");
+                console.log(game);
+            }
+    }
 });
 
