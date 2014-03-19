d72d3e5f47547b3a1c47c7eda7ce762747984568
ldd <leonardoldd@gmail.com>
2014-03-18 22:46:35 -0400

    finished preparation for merging, easily changing form 2D to 3D with a Session variable (complexgame)

--- client/game/game.js
+++ client/game/game.js
@@ -1,3 +1,19 @@
+Template.game.helpers({
+    complexGame: function(){
+        return Session.get('complexGame');
+    },
+
+    selectedShip: function(){
+        return Session.get('selectedShip');
+    },
+
+    selectableShip: function(){
+        var player = (Meteor.userId == Session.get('inGame').opponent)? 'opponent': 'challenger'; 
+        return (Session.get('selectedShip').owner == player);
+    }
+});
+
+
 Template.contextMenu.helpers({
     currentTurn: function() {
         return Session.get('currentTurn');
@@ -5,33 +21,29 @@ Template.contextMenu.helpers({
 
     selectedShip: function(){
         return Session.get('selectedShip');
-    },
-    selectableShip: function(){
-        var player = (Meteor.userId == Session.get('inGame').opponent)? 'opponent': 'challenger'; 
-        return (Session.get('selectedShip').owner == player);
-
     }
+
 });
 
 Template.contextMenu.events({
     'click .actionButton' : function(evt) {
         var action = evt.target.id;
 
-        // if (action == 'moveShip'){
+        if (action == 'moveShip'){
 
-        //     canvas.moveShip(Session.get('selectedShip'));
-        //     var m = Session.get('inGame').map;
-        //     m.__proto__ = new Map();
-        //     canvas.setVisibleFromName(m);
-        // }
+            canvas.moveShip(Session.get('selectedShip'));
+            var m = Session.get('inGame').map;
+            m.__proto__ = new Map();
+            canvas.setVisibleFromName(m);
+        }
 
-        // else if (action =='turnShipRight'){
-        //     canvas.rotateShip(Session.get('selectedShip'), new THREE.Vector3(0,1,0), 1.5* Math.PI);
-        // }
+        else if (action =='turnShipRight'){
+            canvas.rotateShip(Session.get('selectedShip'), new THREE.Vector3(0,1,0), 1.5* Math.PI);
+        }
 
-        // else if (action =='turnShipLeft'){ 
-        //     canvas.rotateShip(Session.get('selectedShip'), new THREE.Vector3(0,1,0), 0.5* Math.PI);
-        // }
+        else if (action =='turnShipLeft'){ 
+            canvas.rotateShip(Session.get('selectedShip'), new THREE.Vector3(0,1,0), 0.5* Math.PI);
+        }
 
         console.log('selected action:');
         console.log(action);
