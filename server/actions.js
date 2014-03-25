37beb84c0a31fcbc5aac97a714b7d5fcdd0ae301
John Saigle <john@wpa151037.wireless.mcgill.ca>
2014-03-25 17:51:34 -0400

    Changed the structure of fireWeapon in moves and the whole damaging system in general

--- server/actions.js
+++ server/actions.js
@@ -41,26 +41,51 @@ Meteor.methods({
     turnShipRight: function(gameID, ship) {
         turnShip(gameID, ship, "clockwise");
     },
-    fireCannon: function(gameID, ship, targetPosition){
-        console.log("Got request in game" +gameID+" to shoot "+targetPosition+" a cannon")
-            var game = gameCollection.findOne({_id:gameID});
-            var map = game.map;
-                            map.__proto__ = new Map();
-            map.grid.__proto__ = new Grid();
-            if (game){
-                map.fireCannon(ship, targetPosition);
+    //This method is used to cause damage to other ships -- represents the firing of a cannon, a torpedo, or a mine explosion
+    useWeapon: function(gameID, ship, weaponType, targetPosition){
+        var targetSquare = map.getObjectAtPosition(targetPosition);
+        //Get game info
+        var game = gameCollection.findOne({_id:gameID});
+        var map = mapCollection.findOne({_id:game.mapID});
+        map.__proto__ = new Map();//TODO: this line may need to be altered to comply with map-grid merge
+        map.grid.__proto__ = new Grid();// ^ ditto
 
-                console.log("done with the map operation.");
-                game.map.shipDictionary[ship.id] = ship;
+        //Represents the area of effect as produced by a weapon being used
+        var AoE = [];
 
-                gameCollection.update({_id:gameID}, game);
-                console.log("done updating game, square should be shot")
-                return true;
-            } else {
-                console.log("Error game does not exist");
-                console.log(game);
-                return false;
+        if (game){
+            //Determine which map method to invoke
+            map.fireCannon(ship, targetPosition);
+            switch (weaponType){
+                case "cannon":
+                    console.log("Preparing to fire cannon at position " +targetSquare.coordinateString()+" ("+ship.shipName+")");
+                    map.fireCannon(ship, targetSquare);
+                    map.applyDamage(AoE);
+                    //Simply apply damage to the shipSquare
+                    //Generate a notification
+                    break;
+                case "mineExplosion":
+                    console.log("Preparing to explode mine at position " +targetSquare.coordinateString());
+                    map.explode(targetSquare);
+                    break;
+                case "torpedo":
+                    console.log("Preparing to fire torpedo at position " +targetSquare.coordinateString()+" ("+ship.shipName+")");
+                    AoE = map.fireTorpedo(ship, targetSquare);
+                    map.applyDamage(AoE);
+                    break;
             }
-    }
+//TODO: Move all below this somewhere better
+            console.log("done with the map operation.");
+            game.map.shipDictionary[ship.id] = ship;
+
+            gameCollection.update({_id:gameID}, game);
+            console.log("done updating game, square should be shot")
+                return true;
+        } else {
+            console.log("Error game does not exist");
+            console.log(game);
+            return false;
+        }
+                }
 });
 
