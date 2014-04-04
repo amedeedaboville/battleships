55a82954acaf274e5728f79bf25fc583298c2544
John Saigle <john.saigle@gmail.com>
2014-04-04 16:43:01 -0400

    Made changes to actions and map with end game conditions and apply damage methods

--- server/actions.js
+++ server/actions.js
@@ -6,8 +6,13 @@ var sendGameMessage = function(message) {
 var turnShip = function(map, ship, direction) {
     //console.log("Got request to turn " + ship.id + " in direction " + direction);
     //console.log("This is ship rotation " + ship.orientation + ",and stern: " + ship.sternPosition + ",and bow: " +ship.bowPosition);
-    map.turnShip(ship, direction); 
-    //console.log("done with map operation")
+    var obstruction = map.turnShip(ship, direction); 
+    if (obstruction === undefined) {
+        console.log("Ship " +ship.name + "sucessfully turned.");
+    } else { //Signal both players about the obstruction
+        console.log("Ship " +ship.name + "failed to turn (obstruction).");
+        sendGameMessage("Collision at position: " + obstruction.coordinateString);
+    }
 };
 
 var moveShip = function(map, ship, position) {
@@ -19,6 +24,7 @@ var moveShip = function(map, ship, position) {
 var turnShipLeft = function(map, ship) {
     turnShip(map, ship, Math.PI*1.5);
 };
+
 var turnShipRight = function(map, ship) {
     turnShip(map, ship, Math.PI*0.5);
 };
@@ -31,7 +37,6 @@ var pickupMine = function(map, ship, position) {
     map.pickupMine(ship, position);
 };
 
-
 Meteor.methods({
     completeTurn: function(action, ship, position){
         //get current Game
@@ -40,23 +45,53 @@ Meteor.methods({
         map.__proto__ = new Map();
 
         eval(action)(map, ship, position)
-        
+
         //update the collections
         map.shipDictionary[ship.id] = ship;
         mapCollection.update({_id:game.mapID}, map);
         gameCollection.update({_id:game._id}, game);
         sendGameMessage(action);
-        
-        //changeTurn
-        gameCollection.update({_id: game._id}, {$inc: {turn: 1}}); 
-    },
 
-    //This method is used to cause damage to other ships -- represents the firing of a cannon, a torpedo, or a mine explosion
-    useWeapon: function(gameID, ship, weaponType, targetPosition){
-        var targetSquare = map.getObjectAtPosition(targetPosition);
-        //Get game info
-        var game = gameCollection.findOne({_id:gameID});
-        var map = mapCollection.findOne({_id:game.mapID});
+        var cHasShips = false;
+        var oHasShips = false;
+        var allShips = map.getShips();
+        var winner = "nobody";
+
+        /* GAME OVER CONDITION: Check to see if the players still have at least one ship */
+        for (shipKey in allShips) {
+            var ship = allShips[shipKey];
+            switch (ship.owner) {
+                case "challenger": 
+                    cHasShips = true;
+                    break;
+                case "opponent": 
+                    oHasShips = true;
+                    break;
+            }
+            if (cHasShips == true && oHasShips == true) {
+                winner = "nobody";
+            }
+        }
+
+        //Hopefully these don't ever get set to true at the same time, or else challenger wins by default
+        if (cHasShips == false)
+            winner = "opponent";
+        if (oHasShips == false)
+            winner = "challenger";
+
+        if (winner == "nobody") { //Continue incrementing turns if the game is not over
+            gameCollection.update({_id: game._id}, {$inc: {turn: 1}}); 
+        } else { //the game is over
+            sendGameMessage("Game over! Winner: " +winner);
+        }
+                  },
+
+                      //This method is used to cause damage to other ships -- represents the firing of a cannon, a torpedo, or a mine explosion
+useWeapon: function(gameID, ship, weaponType, targetPosition){
+               var targetSquare = map.getObjectAtPosition(targetPosition);
+               //Get game info
+               var game = gameCollection.findOne({_id:gameID});
+               var map = mapCollection.findOne({_id:game.mapID});
 
         //Represents the area of effect as produced by a weapon being used
         var dangerZone = [];
@@ -68,7 +103,7 @@ Meteor.methods({
             map.shipDictionary[ship.id] = ship;
             switch (weaponType){
                 case "cannon":
-                    console.log("Preparing to fire cannon at position " +targetSquare.coordinateString()+" ("+ship.shipName+")");
+                    console.log("Preparing to fire cannon at target position " +targetSquare.coordinateString()+" ("+ship.shipName+")");
                     map.fireCannon(ship, targetSquare);
                     map.applyDamage(dangerZone);
                     //Simply apply damage to the shipSquare
@@ -79,7 +114,7 @@ Meteor.methods({
                     map.explode(targetSquare);
                     break;
                 case "torpedo":
-                    console.log("Preparing to fire torpedo at position " +targetSquare.coordinateString()+" ("+ship.shipName+")");
+                    console.log("Preparing to fire torpedo at target position " +targetSquare.coordinateString()+" ("+ship.shipName+")");
                     dangerZone = map.fireTorpedo(ship, targetSquare);
                     map.applyDamage(dangerZone);
                     break;
@@ -97,6 +132,6 @@ Meteor.methods({
             console.log(game);
             return false;
         }
-                }
+               }
 });
 
