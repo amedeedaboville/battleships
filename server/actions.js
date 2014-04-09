9f0afe878c43d7691c9fc82f47c00b30934f00f5
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-04-08 22:48:46 -0400

    removed duplicate methods

--- server/actions.js
+++ server/actions.js
@@ -131,23 +131,6 @@ Meteor.methods({
     },
 
 
-//        if (winner == "nobody") { //Continue incrementing turns if the game is not over
-//            //gameCollection.update({_id: game._id}, {$inc: {turn: 1}}); 
-//        } else { //the game is over
-//            sendGameMessage("Game over! Winner: " +winner);
-//        }
-//},
-
-    rearrange: function(isOpponent) {
-        var game = gameCollection.findOne({$and: [{$or: [{opponent :this.userId}, {challenger: this.userId}]}, {active : true} ]}); //Get the player's active game
-        var map = game.map;
-        map.__proto__ = new Map();
-        map.makeShips(true, !isOpponent, isOpponent);
-        map.drawGrid();
-        gameCollection.update({_id:game._id}, game);
-    },
-
-
 
 /**
 * NAME: useWeapon (GameID gameID, Ship ship, String weaponType, Position targetPosition)
