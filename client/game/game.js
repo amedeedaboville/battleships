e8a14eb67a08a58473ab04fa32aab494527bd132
ldd <leonardoldd@gmail.com>
2014-04-08 21:09:09 -0400

    extra )...

--- client/game/game.js
+++ client/game/game.js
@@ -76,7 +76,7 @@ Template.contextMenu.events({
     },
 
     'click #rearrange' : function(evt) {
-        var isOpponent = getCurrentGame().opponent);
+        var isOpponent = getCurrentGame().opponent;
         console.log('rearranging ships');
         Meteor.call('rearrange', isOpponent);
     },
